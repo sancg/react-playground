e93b5bfe8c395f58b285fb19d8971d0541179925
topduh <sanblackx1@gmail.com>
2025-07-06 06:25:29 -0500

    ADD: Use of cslx for conditional styling

--- src/components/navigation/Link.jsx
+++ src/components/navigation/Link.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { useContext } from 'react';
 import NavigationContext from '../../context/navigation';
+import clsx from 'clsx';
+export const Link = ({ to, children, className, ...rest }) => {
+  const { navigate, currentPath } = useContext(NavigationContext);
 
-export const Link = ({ to, children }) => {
-  const { navigate } = useContext(NavigationContext);
+  const baseStyle = 'text-blue-500';
+  const activeStyle = {
+    'font-bold border-l-4 border-blue-500 pl-2': currentPath === to,
+  };
 
   const handleClick = (e) => {
+    if (e.metaKey || e.ctrKey) {
+      return;
+    }
     e.preventDefault();
 
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a
+      onClick={handleClick}
+      className={clsx(baseStyle, className, activeStyle)}
+      {...rest}
+    >
+      {children}
+    </a>
+  );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { AppTest } from './AppTest.jsx';
+import { NavigationProvider } from './context/navigation.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppTest />
+    <NavigationProvider>
+      <AppTest />
+    </NavigationProvider>
   </React.StrictMode>
 );
