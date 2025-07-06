9cca9f60e2031789b8934894da5bc7245c91ec87
topduh <sanblackx1@gmail.com>
2025-07-06 06:28:23 -0500

    NEW: Sidebar and routing in lesson 238. Navigation reusable

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -1,33 +1,19 @@
 import { useState } from 'react';
-import { NavigationProvider } from './context/navigation';
 import NotFound from './pages/NotFound';
-import { Link } from './components/navigation/Link';
 import Route from './components/navigation/Route';
-import { Dropdown } from './components/ui/Dropdown/Dropdown';
 import DropdownPage from './pages/DropdownPage';
+import { Sidebar } from './components/ui/Sidebar';
+import { Layout } from './components/layout/Layout';
 
 export const AppTest = () => {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    // console.log(option);
-    setSelection((_prev) => option);
-  };
-
-  const colors = [
-    { value: '#FF5733', label: 'Bright Red' },
-    { value: '#33FF57', label: 'Neon Green' },
-    { value: '#3357FF', label: 'Electric Blue' },
-  ];
   return (
-    <NavigationProvider>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/dropdown">Dropdown</Link>
+    <Layout>
+      <Sidebar className="" />
+      <div className="col-span-5 p-2">
+        <Route path="/">
+          <DropdownPage />
+        </Route>
       </div>
-      <Route path="/dropdown">
-        <DropdownPage />
-      </Route>
-    </NavigationProvider>
+    </Layout>
   );
 };
