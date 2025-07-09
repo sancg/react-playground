09ec648902d0da76bdaf98748f9036d38161d2e3
topduh <sanblackx1@gmail.com>
2025-07-09 07:59:10 -0500

    NEW: Reusable table based on a config object

--- src/components/Datatable/DataTable.jsx
+++ src/components/Datatable/DataTable.jsx
@@ -1,21 +1,24 @@
-function DataTable({ data }) {
+function DataTable({ data, config }) {
+  const renderHeaders = config.map((head) => {
+    if (head.render) {
+      return head.render(head);
+    }
+  });
   const renderedRows = data.map((fruit) => {
     return (
-      <tr key={fruit.name + fruit.score}>
-        <td>{fruit.name}</td>
-        <td>{fruit.color}</td>
-        <td>{fruit.score}</td>
+      <tr className="border-b" key={fruit.name + fruit.score}>
+        <td className="p-3">{fruit.name}</td>
+        <td className="p-3">
+          <div className={`p-3 m-2 ${fruit.color}`}></div>
+        </td>
+        <td className="p-3">{fruit.score}</td>
       </tr>
     );
   });
   return (
-    <table>
+    <table className="table-auto border-spacing-2">
       <thead>
-        <tr>
-          <td>Fruit</td>
-          <td>Color</td>
-          <td>Score</td>
-        </tr>
+        <tr className="border-b-2">{renderHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
--- src/pages/DatatablePage.jsx
+++ src/pages/DatatablePage.jsx
@@ -2,14 +2,20 @@ import DataTable from '../components/Datatable/DataTable';
 
 function DatatablePage() {
   const data = [
-    { name: 'Orange', color: '#FFA500', score: 5 },
-    { name: 'Apple', color: '#AA4A44', score: 3 },
-    { name: 'Banana', color: '#FFE135', score: 5 },
+    { name: 'Orange', color: 'bg-orange-200', score: 5 },
+    { name: 'Apple', color: 'bg-red-200', score: 3 },
+    { name: 'Banana', color: 'bg-yellow-200', score: 5 },
+  ];
+
+  const config = [
+    { label: 'Name', render: (h) => <th key={h.label}>{h.label}</th> },
+    { label: 'Color', render: (h) => <th key={h.label}>{h.label}</th> },
+    { label: 'Score', render: (h) => <th key={h.label}>{h.label}</th> },
   ];
 
   return (
     <div>
-      <DataTable data={data} />
+      <DataTable data={data} config={config} />
     </div>
   );
 }
